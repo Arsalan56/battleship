4948c0e9f67596a371afb76ae5d4088ddcb9d7ee
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2024-01-06 18:14:19 -0500

    Create code that fills in tiles for the game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+(() => {
+    const container = document.querySelector('.gameboard');
+
+    for (let i = 0; i < 10; i++) {
+        const box = document.createElement('div');
+        box.setAttribute('data', i + 1);
+        container.appendChild(box);
+
+        for (let j = 0; j < 10; j++) {
+            const boxchild = document.createElement('div');
+            boxchild.setAttribute('data', j + 1);
+            box.appendChild(boxchild);
+        }
+    }
+})();
