455f2fb490ce90497bf3f0424eccbefa96382dd4
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2024-01-08 19:37:38 -0500

    Set up game start page
    The page is layed out to make it look better throughout all devices.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import './style.css';
 
 (() => {
-    const container = document.querySelector('.gameboard');
-
-    for (let i = 0; i < 10; i++) {
-        const box = document.createElement('div');
-        box.setAttribute('data', i + 1);
-        container.appendChild(box);
-
-        for (let j = 0; j < 10; j++) {
-            const boxchild = document.createElement('div');
-            boxchild.setAttribute('data', j + 1);
-            box.appendChild(boxchild);
+    const fillBox = (parent) => {
+        for (let i = 0; i < 10; i++) {
+            const box = document.createElement('div');
+            box.setAttribute('data', i + 1);
+            for (let j = 0; j < 10; j++) {
+                const boxchild = document.createElement('div');
+                boxchild.setAttribute('data', j + 1);
+                box.appendChild(boxchild);
+            }
+            parent.appendChild(box);
         }
-    }
+    };
+    fillBox(document.querySelector('.gameboard1'));
+    // fillBox(document.querySelector('.gameboard2'));
 })();
