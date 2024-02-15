30a26fd283fe1df93be7a73d1d400d3b1d5c3b96
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2024-02-14 19:48:59 -0500

    Make the opponent board randomized
    The ai's board can be randomized, but the program doesn't yet check for
    whether or not a ship is already placed at the current location.

--- src/gameloop.js
+++ src/gameloop.js
@@ -1,10 +1,13 @@
+import Gameboard from './gameboard';
+import Ship from './ship';
+
 export default function Game() {
     let mainBoard;
-
+    const oppBoard = Gameboard(10, 10);
     const main = () => {
         let gameOn = true;
         while (gameOn) {
-            if (mainBoard.allSunk()) {
+            if (oppBoard.allSunk() || mainBoard.allSunk()) {
                 gameOn = false;
             }
         }
@@ -12,6 +15,26 @@ export default function Game() {
 
     const randomInt = (max) => Math.floor(Math.random() * max);
 
+    const randomBoard = (size) => {
+        if (size < 2) {
+            return;
+        }
+        if (randomInt(2) === 0) {
+            oppBoard.changeAxis();
+        }
+
+        console.log(Math.floor(size));
+        oppBoard.place(
+            randomInt(10) + 1,
+            randomInt(10) + 1,
+            Ship(Math.floor(size))
+        );
+        if (size === 4 || size === 3.5) {
+            randomBoard(size - 0.5);
+        } else {
+            randomBoard(size - 1);
+        }
+    };
     const start = (gb) => {
         mainBoard = gb;
         // Make the second board visible
@@ -24,6 +47,8 @@ export default function Game() {
         shipPlace.style.position = 'absolute';
         shipPlace.style.visibility = 'hidden';
         document.querySelector('.gameboard1').style.cursor = 'default';
+        randomBoard(5);
+        console.log(oppBoard.getBoard());
         // main();
     };
     return { start };
